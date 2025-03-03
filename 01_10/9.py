15eba50548a81e206f8dc0d0b50a86e009eeafb8
tdorr96 <thomas.dorrington@cantab.net>
2025-03-03 13:20:34 +0000

    problem 9

--- 01_10/9.py
+++ 01_10/9.py
@@ -2,6 +2,7 @@ if __name__ == '__main__':
 
     # Use Euclid's formula for generating Pythagorean triples.
     # m > n > 0
+    # Did not need to multiply through a, b, and c with a k positive integer as triple was primitive
     for n in range(1, 50):
         for m in range(n + 1, 50):
             a = m ** 2 - n ** 2
